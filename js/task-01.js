052623598856deefc8da3a367e03849f82b3e03f
RomanFalin <romanfalin01@gmail.com>
2022-08-13 21:11:42 +0200

    task-01 (start)

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const elements = document.querySelectorAll(".item");
+console.log("Number of categories:", elements.length);
+
+const categories = document.querySelectorAll(".item");
+console.log("Categories:", categories.values)
