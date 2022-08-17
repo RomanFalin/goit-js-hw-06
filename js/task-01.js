ad860653c56ad691e7ea5eb08e090058b3bae45b
RomanFalin <romanfalin01@gmail.com>
2022-08-18 00:09:09 +0200

    task 01 ready

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,12 @@ const itemsNumber = document.querySelectorAll(".item");
 console.log("Number of categories:", itemsNumber.length);
 
 const category = document.querySelectorAll(".item h2");
-const elements = 
+const elementsFirst = itemsNumber[0].querySelector("ul");
+const elementsSecond = itemsNumber[1].querySelector("ul");
+const elementsThird = itemsNumber[2].querySelector("ul");
 console.log("Category:", category[0]);
+console.log("Elements:", elementsFirst.childElementCount);
 console.log("Category:", category[1]);
+console.log("Elements:", elementsSecond.childElementCount);
 console.log("Category:", category[2]);
+console.log("Elements:", elementsThird.childElementCount);
