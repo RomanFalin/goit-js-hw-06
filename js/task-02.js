fa074e8cdc752539b394287559db90fe8c707697
RomanFalin <romanfalin01@gmail.com>
2022-08-18 00:39:42 +0200

    task 02 ready

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,30 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elementFirst = document.createElement("li");
+elementFirst.textContent = ingredients[0];
+elementFirst.classList.toggle("item");
+
+const elementSecond = document.createElement("li");
+elementSecond.textContent = ingredients[1];
+elementSecond.classList.toggle("item");
+
+const elementThird = document.createElement("li");
+elementThird.textContent = ingredients[2];
+elementThird.classList.toggle("item");
+
+const elementFourth = document.createElement("li");
+elementFourth.textContent = ingredients[3];
+elementFourth.classList.toggle("item");
+
+const elementFifth = document.createElement("li");
+elementFifth.textContent = ingredients[4];
+elementFifth.classList.toggle("item");
+
+const elementSixth = document.createElement("li");
+elementSixth.textContent = ingredients[5];
+elementSixth.classList.toggle("item");
+
+const list = document.querySelector("#ingredients");
+list.append(elementFirst, elementSecond, elementThird, elementFourth, elementFifth, elementSixth);
