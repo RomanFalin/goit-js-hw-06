5b4f6976dac9beb00ddfcad98c6d89ba77bef1ca
RomanFalin <romanfalin01@gmail.com>
2022-08-20 21:04:19 +0200

    task 04 ready

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+const addButton = document.querySelector('button[data-action="increment"]');
+const subtarctButton = document.querySelector('button[data-action="decrement"]');
+let value = document.querySelector('#value');
+
+let counterValue = 0;
+
+const addition = () => {
+    counterValue++;
+    value.innerHTML = counterValue;
+}
+
+const subtraction = () => {
+    counterValue--;
+    value.innerHTML = counterValue;
+}
+
+addButton.addEventListener("click", addition);
+subtarctButton.addEventListener("click", subtraction);
